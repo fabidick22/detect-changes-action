44a26cca839ae752d550aae4f4532a45cf7c4c51
fabidick22 <fabidick22@hotmail.es>
2022-06-26 12:24:58 -0500

    refactor: build app with ncc

--- dist/index.js
+++ dist/index.js
@@ -8863,41 +8863,47 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
+async function setOutputs(files) {
+    const pathsChanged = []
+    const filesChanged = []
+    files.forEach(file => {
+        pathsChanged.push(file.filename.split("/").slice(0, -1).join("/"))
+        filesChanged.push(file.filename)
+    })
+    core.setOutput("paths_changed", JSON.stringify(pathsChanged))
+    core.setOutput("file_changed", JSON.stringify(filesChanged))
+}
+
 const main = async () => {
-  try {
-    const path = core.getInput('path', { required: true });
-    const token = core.getInput('token', { required: true });
-    const regExp = RegExp(path)
-    const octokit = new github.getOctokit(token);
+    try {
+        const path = core.getInput('path', {required: false}) || "./"
+        const token = core.getInput('token', {required: true})
+        const pr = github.context.payload.pull_request
+        const regExp = RegExp(path)
+        const octokit = new github.getOctokit(token);
 
-    const response = await octokit.rest.repos.compareCommits({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      base: "HEAD^",
-      head: "HEAD"
-    })
+        const response = await octokit.rest.pulls.listFiles({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: pr.number
+        })
 
-    const filteredFiles = (response.data.files || []).filter(file => {
-        let isMatch = regExp.test(file.filename)
-        console.log(`[${isMatch && '[** match **]'} ${file.filename}`)
-        console.log(`##Path: ${file.filename} matches`)
-        return isMatch
-    })
+        const filteredFiles = (response.data || []).filter(file => {
+            let isMatch = regExp.test(file.filename)
+            console.log(`- ${isMatch} [** match **] ${file.filename}`)
+            return isMatch
+        })
 
-    if(filteredFiles.length === 0){
-        console.log("No matchs found.")
-        console.log(`Raw input: ${directory}`)
-        console.log(`Regex: ${regExp.toString()}`)
-        core.setOutput("hasChanges", false)
+        if (filteredFiles.length === 0) {
+            console.log("No matchs found.")
+            console.log(`Raw input: ${directory}`)
+            console.log(`Regex: ${regExp.toString()}`)
+        }
+        await setOutputs(filteredFiles);
+
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    console.log(`Found a total of ${filteredFiles.length} matches`)
-
-    core.setOutput("hasChanges", true)
-
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 main();
